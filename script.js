354866cb2fec75096b32245ecc73d0c3e121681f
Luis Felipe Apodaca Barrios <dailalotz@gmail.com>
2026-06-08 13:10:20 -0600

    V3 gp
    V3 gp

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 
 const target=new Date('2026-08-01T21:00:00');
-setInterval(()=>{
+function update(){
 let d=target-new Date();
 if(d<0)d=0;
-document.getElementById('days').textContent=Math.floor(d/86400000);
-document.getElementById('hours').textContent=Math.floor(d%86400000/3600000);
-document.getElementById('minutes').textContent=Math.floor(d%3600000/60000);
-document.getElementById('seconds').textContent=Math.floor(d%60000/1000);
-},1000);
+days.textContent=Math.floor(d/86400000);
+hours.textContent=Math.floor(d%86400000/3600000);
+minutes.textContent=Math.floor(d%3600000/60000);
+seconds.textContent=Math.floor(d%60000/1000);
+}
+update();
+setInterval(update,1000);
